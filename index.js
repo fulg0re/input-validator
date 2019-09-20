2c51ed63f499c95fe6af6b9e57e0322272d8ed45
fulg0re <fulg0re.den@gmail.com>
2019-09-20 15:32:31 +0300

    promisify input validation

--- index.js
+++ index.js
@@ -1,96 +1,14 @@
 const typeCheck = require('type-check').typeCheck;
 
-// const validateSchema = {
-//   username: {
-//     type: 'String',
-//     errorMessage: 'Username is not valid'
-//   },
-//   email: {
-//     type: 'Email',
-//     errorMessage: 'Email is not valid'
-//   },
-//   phone: {
-//     type: 'String',
-//     errorMessage: 'Phone is not valid'
-//   }
-// };
-
-// const tchOptions = {
-//   customTypes: {
-//     Email: {
-//       typeOf: 'String',
-//       validate: (x) => x.length > 5
-//     },
-//     String: {
-//       typeOf: 'String',
-//       validate: (x) => x.length > 5
-//     }
-//   }
-// };
-
-const validateInput = (input, schema) => {
-  for (let key in schema) {
-    if (schema.hasOwnProperty(key)) {
-      if (!typeCheck(schema[key].type, input[key], tchOptions)) {
-        return {
-          validation: false,
-          message: schema[key].errorMessage
-        }
-      }
-    }
-  }
-  return {
-    validation: true
-  }
-};
-
-// const mustCheckObj = {
-//   username: 'flg',
-//   email: 'flg@gmail.com',
-//   phone: '+380673515035'
-// };
-
-// const result = validateInput(
-//   mustCheckObj,
-//   validateSchema
-// );
-
-// console.log(result);
-
-const inputValidate = (input, schema, options) => {
-  let result = {
-    validation: true
-  };
-  
+const inputValidate = (input = {}, schema = {}, options = {}) => new Promise((res, rej) => {
   for (let key in schema) {
     if (schema.hasOwnProperty(key)) {
       if (!typeCheck(schema[key].type, input[key], options)) {
-        return {
-          validation: false,
-          message: schema[key].errorMessage
-        }
+        res({ validation: false, message: schema[key].errorMessage });
       }
     }
   }
-  return result;
-};
+  res({ validation: true });
+});
 
 module.exports = { inputValidate };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
